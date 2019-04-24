bb4b354fc8751a08f859a7c9d083eb446edee841
wenlin.zhang <wenlin.zhang@luckincoffee.com>
2019-04-24 15:58:48 +0800

    ddaadada

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        System.out.print("54632");
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
