74410d08ed655b883ac8619a9073c022e36fc7d6
wenlin.zhang <wenlin.zhang@luckincoffee.com>
2019-04-24 16:09:40 +0800

    8888

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
+
         System.out.print("54632");
         System.out.print("7777");
+        System.out.print("8887");
+
         SpringApplication.run(Demo1Application.class, args);
     }
 
